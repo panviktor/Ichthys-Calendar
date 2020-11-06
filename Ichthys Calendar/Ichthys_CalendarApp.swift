b78606fc1390f5d8049556295fcb3bcf6602975e
SwiftViktor <panafviktor@gmail.com>
2020-11-06 12:51:21 +0300

    Initial Commit

---
+++ Ichthys Calendar/Ichthys_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ichthys_CalendarApp.swift
+//  Ichthys Calendar
+//
+//  Created by Viktor on 06.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct Ichthys_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
