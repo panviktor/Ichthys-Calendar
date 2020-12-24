b97f22fba58c0b0e0620ba12e4d862e1ae07645d
panviktor <panafviktor@gmail.com>
2020-12-24 22:28:48 +0300

    add orientation observer

--- Ichthys Calendar/View/RadioView/RadioList.swift
+++ Ichthys Calendar/View/RadioView/RadioList.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct RadioList: View {
     @StateObject var viewModel: RadioViewViewModel
+    @EnvironmentObject var orientationInfo: OrientationInfo
     @Environment(\.colorScheme) var colorScheme
     @State var isPlaying = true
     
@@ -21,77 +22,148 @@ struct RadioList: View {
                         WaveShape()
                             .fill(Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)).opacity(0.3))
                     }
-                    .frame(width: geometry.size.width, height: 20)
+                    .frame(width: geometry.size.width, height: 15)
                     .edgesIgnoringSafeArea(.top)
-                    Spacer()
-                    
-                    Text("Grid 2")
-                    
-                    Spacer()
-                    ZStack {
-                        Rectangle()
-                            .fill(Constant.gradientBackground)
-                            .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            .edgesIgnoringSafeArea(.all)
-                        VStack {
+                  
+                    if orientationInfo.orientation == .portrait {
+                        Spacer()
+                        Text("Grid 2")
+                        Spacer()
+                        
+                        ZStack {
+                            Rectangle()
+                                .fill(Constant.gradientBackground)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .edgesIgnoringSafeArea(.all)
+                            VStack {
+                                ZStack {
+                                    Rectangle()
+                                        .fill(Constant.gradientBackground)
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                    VStack {
+                                        Text("sfdgdfgdfgdfgdfgfdfgdf")
+                                            .font(.callout)
+                                            .padding()
+                                        
+                                        HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10) {
+                                            Button(action: {
+                                                viewModel.previousStation()
+                                            }) {
+                                                Image(systemName: "backward.fill")
+                                                    .foregroundColor(.red)
+                                                    .font(.title)
+                                            }
+                                            .buttonStyle((RadioButtonStyle()))
+                                            
+                                            Button(action: {
+                                                isPlaying.toggle()
+                                                viewModel.togglePlaying()
+                                            }) {
+                                                Image(systemName: isPlaying ? "play.circle.fill" : "pause.circle.fill")
+                                                    .foregroundColor(.red)
+                                                    .font(.title)
+                                            }
+                                            .buttonStyle((RadioButtonStyle()))
+                                            
+                                            Button(action: {
+                                                viewModel.stop()
+                                            }) {
+                                                Image(systemName: "stop.fill")
+                                                    .foregroundColor(.red)
+                                                    .font(.title)
+                                            }
+                                            .buttonStyle((RadioButtonStyle()))
+                                            
+                                            Button(action: {
+                                                viewModel.nextStation()
+                                            }) {
+                                                Image(systemName: "forward.fill")
+                                                    .foregroundColor(.red)
+                                                    .font(.title)
+                                            }
+                                            .buttonStyle((RadioButtonStyle()))
+                                        }
+                                        .padding(.bottom )
+                                    }
+                                }
+                                .cornerRadius(15)
+                                .padding(6)
+                                .modifier(BasicNeumorphicShadow())
+                            }
+                        }
+                        .frame(height: geometry.size.width / 2)
+                        .cornerRadius(15)
+                        .padding(5)
+                    } else {
+                        HStack {
                             ZStack {
                                 Rectangle()
                                     .fill(Constant.gradientBackground)
                                     .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                    .edgesIgnoringSafeArea(.all)
                                 VStack {
-                                    Text("sfdgdfgdfgdfgdfgfdfgdf")
-                                        .font(.callout)
-                                        .padding()
-                                    
-                                    HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10) {
-                                        Button(action: {
-                                            viewModel.previousStation()
-                                        }) {
-                                            Image(systemName: "backward.fill")
-                                                .foregroundColor(.red)
-                                                .font(.title)
+                                    ZStack {
+                                        Rectangle()
+                                            .fill(Constant.gradientBackground)
+                                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                        VStack {
+                                            Spacer()
+                                            Text("sfdgdfgdfgdfgdfgfdfgdf")
+                                                .font(.callout)
+                                                .padding(.vertical)
+                                            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10) {
+                                                Button(action: {
+                                                    viewModel.previousStation()
+                                                }) {
+                                                    Image(systemName: "backward.fill")
+                                                        .foregroundColor(.red)
+                                                        .font(.title)
+                                                }
+                                                .buttonStyle((RadioButtonStyle()))
+                                                
+                                                Button(action: {
+                                                    isPlaying.toggle()
+                                                    viewModel.togglePlaying()
+                                                }) {
+                                                    Image(systemName: isPlaying ? "play.circle.fill" : "pause.circle.fill")
+                                                        .foregroundColor(.red)
+                                                        .font(.title)
+                                                }
+                                                .buttonStyle((RadioButtonStyle()))
+                                                
+                                                Button(action: {
+                                                    viewModel.stop()
+                                                }) {
+                                                    Image(systemName: "stop.fill")
+                                                        .foregroundColor(.red)
+                                                        .font(.title)
+                                                }
+                                                .buttonStyle((RadioButtonStyle()))
+                                                
+                                                Button(action: {
+                                                    viewModel.nextStation()
+                                                }) {
+                                                    Image(systemName: "forward.fill")
+                                                        .foregroundColor(.red)
+                                                        .font(.title)
+                                                }
+                                                .buttonStyle((RadioButtonStyle()))
+                                            }
+                                            .padding(.bottom, 5)
                                         }
-                                        .buttonStyle((RadioButtonStyle()))
-                                        
-                                        Button(action: {
-                                            isPlaying.toggle()
-                                            viewModel.togglePlaying()
-                                        }) {
-                                            Image(systemName: isPlaying ? "play.circle.fill" : "pause.circle.fill")
-                                                .foregroundColor(.red)
-                                                .font(.title)
-                                        }
-                                        .buttonStyle((RadioButtonStyle()))
-                                        
-                                        Button(action: {
-                                            viewModel.stop()
-                                        }) {
-                                            Image(systemName: "stop.fill")
-                                                .foregroundColor(.red)
-                                                .font(.title)
-                                        }
-                                        .buttonStyle((RadioButtonStyle()))
-                                        
-                                        Button(action: {
-                                            viewModel.nextStation()
-                                        }) {
-                                            Image(systemName: "forward.fill")
-                                                .foregroundColor(.red)
-                                                .font(.title)
-                                        }
-                                        .buttonStyle((RadioButtonStyle()))
                                     }
-                                    .padding(.bottom )
+                                    .cornerRadius(15)
+                                    .padding(6)
+                                    .modifier(BasicNeumorphicShadow())
                                 }
                             }
+                            .frame(width: geometry.size.width / 1.8)
                             .cornerRadius(15)
-                            .padding(6)
-                            .modifier(BasicNeumorphicShadow())
+                            .padding(5)
+                            
+                          Spacer()
                         }
                     }
-                    .frame(height: geometry.size.width / 2)
-                    .cornerRadius(15)
-                    .padding(5)
                 }
             }
         }
